6c87647bbf2eea558a8ae58b4d892866f4ef387c
Karthik-VG <karthikvg.engineer@gmail.com>
2022-03-29 09:31:44 +0530

    updated stage 01

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -2,10 +2,9 @@ import logging
 import os
 import argparse
 import torch
+from pathlib import Path
 from torch.utils.data import DataLoader
 from torchvision import transforms, datasets
-
-
 
 from src.utils.common import create_directories, read_yaml
 
@@ -26,15 +25,39 @@ def main(config_path):
     data_folder_path = content['Data']['root_data_folder']
     create_directories([data_folder_path])
     logging.info(f"getting data")
-    train_data = datasets.FashionMNIST(root=data_folder_path, train=True, download=True,
-                                        transform=transforms.ToTensor())
-    test_data = datasets.FashionMNIST(root=data_folder_path, train=False, download=True, transform= transforms.ToTensor())
+
+    train_tranforms = transforms.Compose([
+                                      transforms.Resize(content["params"]["IMAGE_SIZE"]),
+                                      transforms.RandomRotation(degrees=20),
+                                      transforms.RandomCrop(224),
+                                      transforms.RandomHorizontalFlip(),
+                                      transforms.ToTensor(),
+    ])
+
+    test_tranforms = transforms.Compose([
+                                      transforms.Resize(content["params"]["IMAGE_SIZE"]),
+                                      transforms.ToTensor(),
+
+    ])
+    
+    train_path = Path("Training")
+    
+    test_path = Path("Testing")
+    
+    train_data =  datasets.ImageFolder(root=train_path, transform=train_tranforms)
+
+    test_data = datasets.ImageFolder(root=test_path, transform=test_tranforms)
+    
     given_label = train_data.class_to_idx
+
     label_map = {val: key for key, val in given_label.items()}
+
     logging.info(f"data is available at {data_folder_path}")
 
     logging.info(f"getting dataloader")
+
     train_data_loader = DataLoader(dataset=train_data, shuffle=True, batch_size=content['params']['BATCH_SIZE'])
+    
     test_data_loader = DataLoader(dataset=test_data, batch_size=content['params']['BATCH_SIZE'], shuffle=False)
 
     return train_data_loader, test_data_loader, label_map
